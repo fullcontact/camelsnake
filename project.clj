9864a62f215deefc99994586c25e254f591bdcc5
Karlis Lauva <k@karlis.me>
2016-06-21 14:14:38 -0600

    update to v0.9.0

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject fullcontact/camelsnake "0.1.1-SNAPSHOT"
+(defproject fullcontact/camelsnake "0.9.1-SNAPSHOT"
   :description "String and keyword transformation between cases."
   :url "https://github.com/fullcontact/camelsnake"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :lein-release {:deploy-via :shell
-                 :shell ["lein" "deploy" "clojars"]}
-  :plugins [[lein-midje "3.1.3"]
-            [lein-release "1.0.5"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
+  :java-source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :aot :all
+  :plugins [[lein-midje "3.1.3"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
